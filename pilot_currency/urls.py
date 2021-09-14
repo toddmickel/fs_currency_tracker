58d21bbc58a12862146321346b23899d2dbaa74e
Todd Mickel <todd@toddmickel.com>
2021-09-13 20:56:32 -0600

    Started working on currency board.

--- pilot_currency/urls.py
+++ pilot_currency/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CurrencyDetailView
+from .views import CurrencyDetailView, CurrencyBoardView
 
 urlpatterns = [
     path('', CurrencyDetailView.as_view(), name='currency_detail'),
+    path('board/', CurrencyBoardView.as_view(), name='currency_board')
 ]
