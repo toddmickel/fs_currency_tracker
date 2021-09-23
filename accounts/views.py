53cb88388c382f0833860d317644054703306b98
Thadeus Siwecki <thad.siwecki@gmail.com>
2021-09-22 20:58:47 -0500

    added creat user

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import ProfileForm
 from django.urls import reverse_lazy
@@ -8,7 +8,7 @@ from .models import CustomUser
 
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
